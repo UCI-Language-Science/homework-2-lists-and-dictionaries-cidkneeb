c38919ce0eac6a9e15acaa5ae0db4b166137d5f5
sydney <125717585+cidkneeb@users.noreply.github.com>
2025-01-24 19:33:27 +0000

    duplicate remover implemented.

--- duplicate_remover.py
+++ duplicate_remover.py
@@ -7,8 +7,17 @@
 # 
 def duplicate_remover():
     duplicates_list = [1, 4, 3, 4, 2, 5, 1, 2, 7, 9, 4]
-    
-    # YOUR CODE GOES HERE
+    lst = []
+    for i in duplicates_list:
+        if i in lst:
+            continue
+        else:
+            lst.append(i)
+    print(lst)
+
+    # for i in range(len(duplicates_list)):
+    #     print(i)
+
 
 if __name__ == "__main__":
     duplicate_remover()
